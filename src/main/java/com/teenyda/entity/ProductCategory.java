fbec1a29f141d62c85970c1641e5bf01527a1f2c
teenyda <teenyda@gmail.com>
2020-10-19 15:15:28 +0800

    产品查询语句修改

--- src/main/java/com/teenyda/entity/ProductCategory.java
+++ src/main/java/com/teenyda/entity/ProductCategory.java
@@ -1,5 +1,7 @@
 package com.teenyda.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,6 +11,7 @@ import java.util.Date;
  * @author makejava
  * @since 2020-10-09 17:10:53
  */
+@Data
 public class ProductCategory implements Serializable {
     private static final long serialVersionUID = -39528109752538233L;
 
@@ -27,60 +30,5 @@ public class ProductCategory implements Serializable {
     private Integer updateUserId;
 
 
-    public Integer getProductCategoryId() {
-        return productCategoryId;
-    }
-
-    public void setProductCategoryId(Integer productCategoryId) {
-        this.productCategoryId = productCategoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(Integer sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getUpdateUserId() {
-        return updateUserId;
-    }
-
-    public void setUpdateUserId(Integer updateUserId) {
-        this.updateUserId = updateUserId;
-    }
 
 }
